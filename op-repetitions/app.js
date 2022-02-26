66913deb40a78f8df4d1720903aea00b62d55d31
Leandro Rocha Pereira <lrp.0689@gmail.com>
2022-02-25 23:45:33 -0300

    Op App.js

---
+++ op-repetitions/app.js
@@ -0,0 +1,81 @@
+/* Operadores e funções
+Operador de atribuição: (=) 
+Este operador é o mais comum no dia-a-dia da 
+programação, ele serve para atribuir valor a 
+uma variável/constante
+Ex:
+const valido = false;
+let idade = 18;
+const mensagem = "Favor informar dados válidos";
+------------------------------------------------------------------------
+Operador de comparação: (== ou ===)
+Assim como a atribuição esse operador utiliza o =, porém nesse caso
+podem ser dois ou três ==, === fazendo uma comparação e não uma atribuição
+Ex:
+if (1=="1") {
+    console.log("São equivalentes"); 
+    == usamos para uma comparação simples, um lado ao outro, 
+    neste caso, o 1 typo number tem o mesmo VALOR do 1 string
+}
+if (1===1) {
+    console.log("São idênticos");
+    === usamos para comparar os TIPOS E O VALOR, estamos comparando
+    1 numero com 1 numero, então são idênticos
+}
+Existe também um operador para verificar se os dados comparados são
+diferentes, este operador pode ser o != ou !==
+Ex:
+if (1 != 2){
+    console.log("1 é diferente de 2");
+}
+if (1 !== "1"){
+    console.log("1 numérico é diferente de '1' como string");
+}
+Demais operadores de comparação
+> Compara se o valor da esquerda é maior que o da direita retornando true/false
+>= Compara se o valor da esquerda é maior ou igual ao da direita retornando true/false
+< Compara se o valor da esquerda é menor que o da esquerda retornando true/false
+<= Compara se o valor da esquerda é menor ou igual ao da esquerda retornando true/false
++ efetua adição entre dois valores
+- efetua subtração entre dois valores
+* efetua uma multiplicação entre dois valores
+/ efetua uma divisão
+% efetua um módulo entre dois valores
+++ operador de incremento de 1 no valor passado
+-- efetua uma subtração de 1 no valor passado
+------------------------------------------------------------------------
+Métodos & Funções
+Assim como já falamos de variáveis e constantes, também já falamos dos nossos métodos e funções.
+Basicamente são comportamentos que nossa classe deseja realizar separadamente
+    Um nome que auxilia a identificar a função e chama-la
+    A palavra reservada FUNCTION ou a sintaxe double arrow function
+    Uma lista de variáveis e parâmetros de entrada que a função precisa para funcionar
+    Um escopo onde será executada as instruções respectivas dessa função
+
+A função inicia com a palavra reservada function, depois vem o nome e entre parênteses a 
+lista de parâmetros de entrada para executar essa operação
+Ex:
+function validar(valido){
+    if(valido == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const teste = false;
+console.log(validar(teste));
+.........................................................................
+A sintaxe double arrow, inicia com a declaração de uma variável, depois vem entre parênteses
+a lista de parâmetros de entrada para executar essa operação e o operador =>
+Ex:
+const validar = (valido) => {
+    if (valido == true){
+        return false;
+    } else {
+        return false;
+    }
+}
+const teste = false;
+console.log(validar(teste));
+.........................................................................
+*/
