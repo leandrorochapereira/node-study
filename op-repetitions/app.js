2574ed31fd8846f152d94402fd6339490e353bb7
Leandro Rocha Pereira <lrp.0689@gmail.com>
2022-02-26 00:43:11 -0300

    Calculadora.app

--- op-repetitions/app.js
+++ op-repetitions/app.js
@@ -79,3 +79,64 @@ const teste = false;
 console.log(validar(teste));
 .........................................................................
 */
+/*1º exercício, criar um programa que recebe 2 números e um operador
+matemático e com isso executa o cálculo corretamente */
+
+const readLine = require('readline').createInterface ({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const validarNumeroInformado = (numero) => {
+    const resultado = Number.parseFloat(numero);
+    if (!resultado) {
+        console.log(`Número informado não é válido.`);
+    }
+    return resultado;
+    
+}
+
+const validarOperador = (operador) => {
+    switch (operador) {
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+        case '%':
+            return operador;    
+        default:
+            console.log('operador informado é invalido');
+            return null;
+    }
+}
+
+readLine.question('Favor informar um número: ', (numero1) =>{
+    const numeroValidado1 = validarNumeroInformado(numero1);
+
+    if(numeroValidado1) {
+        readLine.question('Favor informar outro número: ', (numero2) =>{
+            const numeroValidado2 = validarNumeroInformado(numero2);
+
+            if (numeroValidado2) {
+                readLine.question('Favor informar o operador: ', (operador) =>{
+                    const operadorValidado = validarOperador(operador);
+                    if (operadorValidado) {
+                        switch (operadorValidado) {
+                            case '+': console.log(`Operador selecionado adição, resultado ${numeroValidado1} + ${numeroValidado2} = ${numeroValidado1 + numeroValidado2}`);
+                                break;
+                            case '-': console.log(`Operador selecionado subtração, resultado ${numeroValidado1} - ${numeroValidado2} = ${numeroValidado1 - numeroValidado2}`);
+                                break;
+                            case '*': console.log(`Operador selecionado multiplicação, resultado ${numeroValidado1} * ${numeroValidado2} = ${numeroValidado1 * numeroValidado2}`);
+                                break;
+                            case '/': console.log(`Operador selecionado divisão, resultado ${numeroValidado1} / ${numeroValidado2} = ${numeroValidado1 / numeroValidado2}`);
+                                break;
+                            case '%': console.log(`Operador selecionado modulo, resultado ${numeroValidado1} % ${numeroValidado2} = ${numeroValidado1 % numeroValidado2}`);
+                                break;
+                            default: break;
+                        }
+                    }
+                })
+            }
+        })
+    }
+})
